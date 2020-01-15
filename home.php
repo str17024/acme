f3f21c0927eeae9a0a6ac074ea9bec7614bcebd2
Ken Strobell <35313618+str17024@users.noreply.github.com>
2020-01-14 19:47:25 -0500

    Update home.php

--- home.php
+++ home.php
@@ -14,21 +14,21 @@
  
     <!-- external style references in the proper cascading order   -->
   <link href="https://fonts.googleapis.com/css?family=Tangerine" rel="stylesheet">  <!-- Google API font reference -->
-  <link href="/css/-normalize.css" rel="stylesheet"> <!-- normalize useragent/browser defaults -->
+  <link href="/css/normalize.css" rel="stylesheet"> <!-- normalize useragent/browser defaults -->
   <link href="main.css" rel="stylesheet">   <!-- default styles - small/phone views -->
-  <link href="/css/-medium-5.css" rel="stylesheet">  <!-- medium/tablet views -->
-  <link href="/css/-large-5.css" rel="stylesheet">   <!-- large/wide/desktop views -->
-  <link href="/css/-small.css" rel="stylesheet">   <!-- small views --> 	
+  <link href="/css/medium-5.css" rel="stylesheet">  <!-- medium/tablet views -->
+  <link href="/css/large-5.css" rel="stylesheet">   <!-- large/wide/desktop views -->
+  <link href="/css/small.css" rel="stylesheet">   <!-- small views --> 	
 </head>
     
-<body>
+    
     
     
 <header>
     	  
     <img src="images/site/logo.gif" alt="ACME Buy Here. Eat Here" width="6" height="3" >
-    <h1>ACME Test Site Ken S....</h1>
-    <div class="subheading">Kaboom!</div>
+    <h1>ACME</h1>
+    <div class="subheading">MY ACCOUNT</div>
 	
   </header>
 
@@ -44,28 +44,64 @@
 	  <a href="NHTSAapprovedseatbelts.html">NHTSA approved Seat Belts</a>
 	  <a href="mobilelaunchstandincluded.html">Mobile Launch Stand Included</a>
 	  <a href="contactus.html">Contact Us</a>
-      <a href="/acme/cart/"><img id="actionbtn" alt="Add to cart button" src="/images/site/iwantit.gif"></a>
+      
 </nav>		
     
     
- <div id="main">		
+    
   
     
   <main>
-	  <p>
-    <h2 text-align="center">The Acme Test Page Kens</h2>
 	  
-	  
+    <h2 text-align="center">WELCOME TO ACME!!</h2>
+    <!-- <img src="images/site/rocketfeature.jpg" alt="Therocketfeatureone"> -->
+    
+      <div class="acmerocketfloat">
+           <p>Quick Lighting Fuse<br>NHTSA Approved Seat Belts<br>Mobile Launch Stand Included</p>
+          <a href="/acme/cart/"><img id="actionbtn" alt="Add to cart button" src="./images/site/iwantit.gif"></a>
+          
+      </div>
+    </main>
+     <div class="body">     
+          
+        
+          <div >Featured Recipies!</div>
+        
+          <div class="gallery">
+  <a target="_blank" href="">
+    <img src="./images/recipes/bbqsand.jpg" alt="bbq" >
+  </a>
+  <div class="desc">Pulled Roadrunner BBQ</div>
+</div>
 
-<div class="weathersummary">
-    <div class="franklinimage">
-      <img src="images/site/rocketfeature.jpg" alt="Therocketfeatureone">
-    </div>
-      <aside class="sidebar1">
-      <h3 >Bike Adventures!</h3>   
+<div class="gallery">
+  <a target="_blank" href="">
+    <img src="./images/recipes/potpie.jpg" alt="potpie">
+  </a>
+  <div class="desc">Roadrunner Pot Pie</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="">
+    <img src="./images/recipes/soup.jpg" alt="soup">
+  </a>
+  <div class="desc">Roadrunner Soup</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="">
+    <img src="./images/recipes/taco.jpg" alt="tacos">
+  </a>
+  <div class="desc">Roadrunner Tacos</div>
+</div>
+          
+
+    
+<div class="acmereviewsfloat">
+       
       
-      <div class="highlow">
-        <p class="box1">food and reviews!</p>
+      <div>
+        <p>ACME Rocket Reviews!</p>
            <ul>
     <li>"I don't know how I ever caught roadrunners before this." (4/5)</li>
     <li>"That thing was fast!" (4/5)</li>
@@ -73,40 +109,16 @@
     <li>"I didn't even have to pull the meat apart." (4.5/5)</li>
     <li>"I'm on my thirtieth one. I love these things!" (5/5)</li>
   </ul>
-        <div class="box2">recipies!</div>  
-           <ul>
-    <li>Pulled Roadrunner BBQ</li>
-    <li>Roadrunner Pot Pie</li>
-    <li>Roadrunner Soup</li>
-    <li>Roadrunner Tacos</li>
-    <li>yum</li>
-  </ul>
-        <div class="box3">things!</div>  
-          <p></p>
-        <div>Did I mention we offer the best food adventures ever?!</div>
-        <div>Come check us out!</div>    
-          
-      </div> 
-    </aside>
-        
-      </div>           
+    </div>
+               </div>
     
-
- 
-
-
-<div>
-
-
-</div>
-</main>
     
-
-<p>
+         
+    </div>
     
-</body>
     
- <footer>
+  
+ <footer id="footer">
      
             <aside>
                 3950 Valley Commons Drive<br>
@@ -114,20 +126,11 @@
                 (111) 222 3333 <br>
                 kens@testacmepage.com
             </aside>
-            <p class="footer-bar">&copy; ACME, All rights reserved.
-All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed. <span id="currentdate"></span></p>
+            <p >&copy; ACME, All rights reserved.
+All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed. ></p>
 </footer>
-
-  
-
-  <script src="/js/hamburger.js"></script>
-  <script src="/js/hideMenu.js"></script>
-  <script src="/js/jquery.min.js"></script>
-  <script src="/js/currentdate.js"></script>
-  <script src="/js/windchill.js"></script>
-
-
-
     
+    
+
 
 </html>
