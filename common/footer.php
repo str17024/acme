f723149fe05dd44b94bfd9c56db83d336a1811a1
Ken Strobell <35313618+str17024@users.noreply.github.com>
2020-01-16 21:01:01 -0500

    Add files via upload

---
+++ common/footer.php
@@ -0,0 +1,8 @@
+<footer id="footer">
+     
+            <aside>
+                Last Updated Jan 19 2020<br>
+            </aside>
+            <p >&copy; ACME, All rights reserved.
+All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed. </p>
+</footer>
---
+++ common/header.php
@@ -0,0 +1,22 @@
+<header>
+    	  
+    <div class="acmelogo"> <img src="./images/site/logo.gif" alt="ACME Buy Here. Eat Here"> </div>
+    
+    <div class="myaccount"><img src="./images/site/account.gif" alt="accountimage">MY ACCOUNT</div>
+	
+  </header>
+
+  <!-- site navigation use placeholder references -->
+	
+
+
+  <nav class="topnavacme">
+   
+      <a class="active" href="https://str17024.github.io/acme/">Home</a>
+	  <a href="acmerocket.html">Cannon</a>
+	  <a href="quicklightingfuse.html">Explosive</a>
+	  <a href="NHTSAapprovedseatbelts.html">Misc</a>
+	  <a href="mobilelaunchstandincluded.html">Rocket</a>
+	  <a href="contactus.html">Trap</a>
+      
+</nav>	
