a08e48dd4bd8770835f7c08fe728833de73a3579
Ken Strobell <35313618+str17024@users.noreply.github.com>
2020-01-20 21:47:06 -0500

    Create navigation.php

---
+++ common/navigation.php
@@ -0,0 +1,10 @@
+<nav class="topnavacme">
+   
+      <a class="active" href="https://str17024.github.io/acme/">Home</a>
+	  <a href="acmerocket.html">Cannon</a>
+	  <a href="quicklightingfuse.html">Explosive</a>
+	  <a href="NHTSAapprovedseatbelts.html">Misc</a>
+	  <a href="mobilelaunchstandincluded.html">Rocket</a>
+	  <a href="contactus.html">Trap</a>
+      
+</nav>	
