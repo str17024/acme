dc454fa3e03af285322890a25b84bb031d8ec46c
Ken Strobell <35313618+str17024@users.noreply.github.com>
2020-01-20 21:51:50 -0500

    Update navigation.php

--- common/navigation.php
+++ common/navigation.php
@@ -1,10 +1,10 @@
 <nav class="topnavacme">
    
-      <a class="active" href="https://str17024.github.io/acme/">Home</a>
-	  <a href="acmerocket.html">Cannon</a>
-	  <a href="quicklightingfuse.html">Explosive</a>
-	  <a href="NHTSAapprovedseatbelts.html">Misc</a>
-	  <a href="mobilelaunchstandincluded.html">Rocket</a>
-	  <a href="contactus.html">Trap</a>
+      <a class="active" href="https://str17024.github.io/acme/" title="home">Home</a>
+	  <a href="acmerocket.html" title="cannon">Cannon</a>
+	  <a href="quicklightingfuse.html" title="explosives">Explosive</a>
+	  <a href="NHTSAapprovedseatbelts.html" title="misc">Misc</a>
+	  <a href="mobilelaunchstandincluded.html" title="rocket">Rocket</a>
+	  <a href="contactus.html" title="trap">Trap</a>
       
 </nav>	
