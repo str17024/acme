5cd9367d633aa7a7e594fcb76e93da08ab1b41f8
Ken Strobell <35313618+str17024@users.noreply.github.com>
2020-01-20 22:10:47 -0500

    Update header.php

--- common/header.php
+++ common/header.php
@@ -1,5 +1,14 @@
 <!doctype html>
 <html lang="en-us">
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ACME Test Site Ken S</title>
+  <meta name="author" content="Ken Strobell">
+  <meta name="the official page Acme." content="TheAcmeTest site">
+  <link href="https://fonts.googleapis.com/css?family=Tangerine" rel="stylesheet">  <!-- Google API font reference -->
+  <link href="./css/main.css" rel="stylesheet">   <!-- default styles - small/phone views -->
+ 
+</head>
 <header>
     	  
     <div class="acmelogo"> <img src="./images/site/logo.gif" alt="ACME Buy Here. Eat Here"> </div>
